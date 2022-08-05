11335a5f659fe8d2e9d8e2788e73b25e0a81bbab
ChyunKim <co1995@naver.com>
2022-08-05 18:26:40 +0900

    docs: Edit

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,40 +1,41 @@
 import './App.css';
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
   const [input, setInput] = React.useState('')
   const [list, setList] = React.useState([])
+  const [boolcheck, setCheck] = React.useState(false)
 
-  // input 이벤트 발생
-  const eventInput = (e) => {
-    // 키가 입력되면 input state 변경되도록
-    setInput(e.target.value)
-
-    // 엔터를 입력하면 입력하면 입력된 value 값을 확인하여 입력이 안되었다면 경고창출력
-    // 입력이 되었다면 input value 값 todo 리스트에 추가하는 함수 호출
-    if(e.key === 'Enter'){
-        e.target.value.length === 0 ? alert('할일을 입력하세요!') : listAdd(input)
-    }
+  const ListView = () => {
+    return (
+      <ul>
+        {list.map(item => 
+            <li key = {item}><input className = "check" type = "checkbox" checked = {boolcheck}/>{item}
+            <button className='remove'></button>
+          </li>)}
+      </ul>
+    )
   }
 
-  // todo리스트 추가 함수
-  const listAdd = (value) => {
+  const eventClick = (e) => {
+      input.length === 0 &&  alert('할일을 입력하세요!')
+      input.length !== 0 && listAdd(input) || setInput('')
+  }
 
+  const listAdd = (value) => {
     // 기존 list에 배열을 concat을 사용하여 list 추가
     const todo = list.concat(value)
-
-    // 추가된 리스트 todo를 state를 이용하여 상태 변경
     setList(todo)
-    setInput('')
   }
+
 
   return (
     <div className = "App">
       <div className ="todo-container">
         <h1>TODO List</h1>
-        <input type = "text" onKeyDown = {(e)=>eventInput(e)} />
-        <ul>{list.map(item => <li key = {item}><input type = "checkbox"/>{item}</li>)}</ul>
-        <button className="plusbtn">+</button>
+        <input value = {input} type = "text" onChange={(e)=>setInput(e.target.value)} />
+        <button className="plusbtn" onClick={eventClick}>+</button>
+        <ListView />
       </div>
     </div>
   );
